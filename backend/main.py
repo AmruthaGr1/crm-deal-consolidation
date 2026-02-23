0dd3e44c5519dc9e7c8f21f119b5b260d83d4e66
AmruthaGr1 <amrutha01.am@gmail.com>
2026-02-23 10:51:53 -0500

    Enabled CORS for deployment in main.py

--- backend/main.py
+++ backend/main.py
@@ -100,6 +100,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# your routes below
+@app.get("/")
+def root():
+    return {"message": "Hello"}
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[],
@@ -109,18 +113,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# your routes below
-@app.get("/")
-def root():
-    return {"message": "Hello"}
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # -----------------------------
 # Helpers
