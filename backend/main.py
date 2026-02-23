c2a3301fd740eda75b31f5681a67614cfe61b329
AmruthaGr1 <amrutha01.am@gmail.com>
2026-02-23 11:13:56 -0500

    CORS

--- backend/main.py
+++ backend/main.py
@@ -104,10 +104,14 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello"}
+
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[],
-    allow_origin_regex=r"^https://.*\.vercel\.app$",
+    allow_origins=[
+        "https://crm-deal-consolidation1-frp5rjmjy-amrutha01am-6020s-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
